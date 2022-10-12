028e45c7a8850edb2275a4d3bffc30046bd002c4
dumbcoder42 <lyk42@163.com>
2022-10-12 11:35:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-iccjzq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-iccjzq)
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1 className="font-bold text-green-400 underline">Hello world!</h1>;
+}
